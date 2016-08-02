6124849fc2e78327b4b810d3986dcab029442fd3
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-02 10:42:42 +0200

    Plot a chunk of interarrivals

--- bin/analyze.py
+++ bin/analyze.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
-import math
-import matplotlib.pyplot as plt
+import math, sqlite3
+import matplotlib.pyplot as pp
 import numpy as np
-import sqlite3
+
+def display(data, limit=10000):
+    limit = len(data) if limit is None else limit
+    figure()
+    pp.semilogy(data[:limit])
+    pp.title('Interarrivals, %d of %d samples' % (limit, len(data)))
+
+def figure():
+    pp.figure(figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
+
+def histogram(data):
+    figure()
+    pp.hist(np.log10(data), bins=1000, log=True)
+    pp.title('Histogram of interarrivals')
+    pp.xlabel('log(time)')
+    pp.ylabel('log(count)')
 
 def read(path='tests/fixtures/google.sqlite3'):
     connection = sqlite3.connect(path)
@@ -13,17 +28,16 @@ def read(path='tests/fixtures/google.sqlite3'):
     connection.close()
     return data
 
+def summarize(data):
+    mean, variance = np.mean(data), np.var(data)
+    print('Samples: %d' % len(data))
+    print('Mean: %.4f ± %.4f' % (mean, math.sqrt(variance)))
+    print('Minimum: %e' % np.min(data))
+    print('Maximum: %e' % np.max(data))
+
 data = read()
-mean, variance = np.mean(data), np.var(data)
+summarize(data)
+histogram(data)
+display(data[data > 1e-5])
 
-print('Samples: %d' % len(data))
-print('Mean: %.4f ± %.4f' % (mean, math.sqrt(variance)))
-print('Minimum: %e' % np.min(data))
-print('Maximum: %e' % np.max(data))
-
-plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
-plt.hist(np.log10(data), bins=1000, log=True)
-plt.title('Histogram of interarrivals')
-plt.xlabel('log(time)')
-plt.ylabel('log(count)')
-plt.show()
+pp.show()
