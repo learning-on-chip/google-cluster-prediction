7ebeba63b2a8668cea0806e4e837ca54f8cb7d31
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-05 21:51:37 +0200

    Do a bit of refactoring

--- bin/learn.py
+++ bin/learn.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 
 def assess(f):
     layer_count = 1
-    unit_count = 100
+    unit_count = 200
 
     learning_rate = 1e-4
     train_count = int(1e5)
@@ -24,11 +24,11 @@ def assess(f):
     with graph.as_default():
         x = tf.placeholder(tf.float32, [1, 1, 1], name='x')
         y = tf.placeholder(tf.float32, [1, 1, 1], name='y')
-        (y_hat, l), updates = model_fn(x, y)
+        (y_hat, loss), (start, finish) = model_fn(x, y)
 
         with tf.variable_scope('optimization'):
             trainees = tf.trainable_variables()
-            gradient = tf.gradients(l, trainees)
+            gradient = tf.gradients(loss, trainees)
             optimizer = tf.train.AdamOptimizer(learning_rate)
             train = optimizer.apply_gradients(zip(gradient, trainees))
 
@@ -40,24 +40,28 @@ def assess(f):
         session.run(initialize)
         taken_count = 0
 
-        parameter_count = np.sum([int(np.prod(trainee.get_shape())) for trainee in trainees])
-        print('Learning {} parameters...'.format(parameter_count))
+        learn_count = np.sum([int(np.prod(t.get_shape())) for t in trainees])
+        print('Learning {} parameters...'.format(learn_count))
         print('%12s %12s %12s' % ('Iterations', 'Samples', 'Loss'))
+        fetches = {'finish': finish, 'train': train, 'loss': loss}
+        feeds = {start: np.zeros(start.get_shape(), dtype=np.float32)}
         for i in range(train_count):
-            x_observed, y_observed, taken_count = stream_fn(taken_count)
-            results = session.run([l, train] + updates, {x: x_observed, y: y_observed})
+            feeds[x], feeds[y], taken_count = stream_fn(taken_count)
+            results = session.run(fetches, feeds)
+            feeds[start] = results['finish']
             if taken_count % monitor_period != 0: continue
-            print('%12d %12d %12.2e' % (i + 1, taken_count, results[0]))
+            print('%12d %12d %12.2e' % (i + 1, taken_count, results['loss']))
 
         Y_observed = np.zeros([imagine_count, 1])
         Y_imagined = np.zeros([imagine_count, 1])
-        x_imagined = y_observed
+        fetches = {'finish': finish, 'y_hat': y_hat}
+        feeds = {start: feeds[start], x: feeds[y]}
         for i in range(imagine_count):
             _, y_observed, taken_count = stream_fn(taken_count)
             Y_observed[i] = y_observed[0]
-            results = session.run([y_hat] + updates, {x: x_imagined})
-            Y_imagined[i] = results[0][0]
-            x_imagined[0] = results[0][0]
+            results = session.run(fetches, feeds)
+            feeds[start] = results['finish']
+            Y_imagined[i] = feeds[x][0] = results['y_hat'][0]
 
         compare(Y_observed, Y_imagined)
 
@@ -81,43 +85,42 @@ def compare(y, y_hat):
 def model(layer_count, unit_count):
     def compute(x, y):
         with tf.variable_scope('network') as scope:
-            initializer = tf.random_uniform_initializer(-0.5, 0.5)
+            initializer = tf.random_uniform_initializer(-0.05, 0.05)
             cell = tf.nn.rnn_cell.LSTMCell(unit_count, forget_bias=0.0,
                                            initializer=initializer,
                                            state_is_tuple=True)
             cell = tf.nn.rnn_cell.MultiRNNCell([cell] * layer_count,
                                                state_is_tuple=True)
-            start = initiate()
-            h, finish = cell(tf.squeeze(x, squeeze_dims=[1]), start)
-            updates = shortcut(start, finish)
-        return regress(h, y), updates
+            start, state = initialize()
+            output, state = cell(tf.squeeze(x, squeeze_dims=[1]), state)
+            finish = finalize(state)
+        return regress(output, y), (start, finish)
 
-    def initiate():
+    def finalize(state):
+        parts = []
+        for i in range(layer_count):
+            parts.append(state[i].c)
+            parts.append(state[i].h)
+        return tf.pack(parts, name='finish')
+
+    def initialize():
+        start = tf.placeholder(tf.float32, [2 * layer_count, 1, unit_count],
+                               name='start')
+        parts = tf.unpack(start)
         state = []
         for i in range(layer_count):
-            c = tf.get_variable('c{}0'.format(i + 1),
-                                initializer=tf.zeros([1, unit_count]))
-            h = tf.get_variable('h{}0'.format(i + 1),
-                                initializer=tf.zeros([1, unit_count]))
+            c, h = parts[2 * i], parts[2*i + 1]
             state.append(tf.nn.rnn_cell.LSTMStateTuple(c, h))
-        return state
+        return start, state
 
     def regress(x, y):
         with tf.variable_scope('regression') as scope:
-            w = tf.get_variable('w', initializer=tf.truncated_normal([unit_count, 1]))
+            initializer = tf.truncated_normal([unit_count, 1], stddev=0.5)
+            w = tf.get_variable('w', initializer=initializer)
             b = tf.get_variable('b', [1])
             y_hat = tf.squeeze(tf.matmul(x, w) + b, squeeze_dims=[1])
             loss = tf.reduce_sum(tf.square(tf.sub(y_hat, y)))
         return y_hat, loss
-
-    def shortcut(start, finish):
-        updates = []
-        for i in range(layer_count):
-            updates.append(tf.assign(start[i].c, finish[i].c,
-                                     name="c{}0-update".format(i + 1)))
-            updates.append(tf.assign(start[i].h, finish[i].h,
-                                     name="h{}0-update".format(i + 1)))
-        return updates
 
     return compute
 
