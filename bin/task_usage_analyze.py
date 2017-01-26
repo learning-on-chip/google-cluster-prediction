1417d6dff1c82fe166ef5fa6f3333bb1b5f7d40d
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-01-26 08:52:42 +0100

    Flesh out task_usage_analyze.py

--- bin/task_usage_analyze.py
+++ bin/task_usage_analyze.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
+
 import glob
+import numpy as np
+
+from task_usage import Database
 
 def main(data_path):
-    count = len(glob.glob('{}/**/*.sqlite3'.format(data_path)))
-    print('Count: {}'.format(count))
+    paths = glob.glob('{}/**/*.sqlite3'.format(data_path))
+    for path in paths:
+        data = Database(path).count_job_task_samples()
+        print('[{}] Jobs: {:2}, tasks: {:6}, samples: {:10}'.format(
+            path, len(np.unique(data[:, 0])), data.shape[0],
+            np.sum(data[:, 2])))
+    print('Count: {}'.format(len(paths)))
 
 if __name__ == '__main__':
     assert(len(sys.argv) == 2)
