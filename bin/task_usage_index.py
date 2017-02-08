094cb428316ac0fceb0178d5a507e746550f4509
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-02-08 14:07:39 +0100

    Print progress from the indexing script

--- bin/task_usage_index.py
+++ bin/task_usage_index.py
@@ -8,21 +8,25 @@ import numpy as np
 
 import task_usage
 
-def main(data_path, index_path):
-    count = 0
+def main(data_path, index_path, report_each=10000):
+    print('Looking for data in "{}"...'.format(data_path))
+    paths = sorted(glob.glob('{}/**/*.sqlite3'.format(data_path)))
+    print('Processing {} databases...'.format(len(paths)))
     index = []
-    for path in sorted(glob.glob('{}/**/*.sqlite3'.format(data_path))):
+    count = 0
+    for path in paths:
         data = task_usage.count_job_task_samples(path)
         for i in range(data.shape[0]):
             index.append({
                 'path': path,
                 'job': int(data[i, 0]),
                 'task': int(data[i, 1]),
-                'count': int(data[i, 2]),
+                'length': int(data[i, 2]),
             })
         count += 1
-        if count % 10000 == 0:
+        if count % report_each == 0:
             print('Processed: {}'.format(count))
+    print('Saving into "{}"...'.format(index_path))
     with open(index_path, 'w') as file:
         json.dump({'index': index}, file, indent=4)
 
