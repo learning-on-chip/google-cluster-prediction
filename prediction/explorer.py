5c601035a66ccdf9807ba0c7858f2a7eee16a169
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-09 15:27:45 +0100

    Start to learn in Explorer

--- prediction/explorer.py
+++ prediction/explorer.py
@@ -10,83 +10,96 @@ import threading
 
 class Explorer:
     def __init__(self, config):
+        self.config = config.learner
+        self.tuner = getattr(tuner, config.tuner.name)
+        self.tuner = self.tuner(**config.tuner.options)
+        self.resource_scale = config.max_iteration_count / self.tuner.resource
         self.sampler = Sampler(config.sampler)
-        self.learner_config = config.learner
-        self.tuner_config = config.tuner
         self.semaphore = threading.BoundedSemaphore(config.concurrent_count)
         self.agents = {}
 
     def run(self):
-        tuner_ = getattr(tuner, self.tuner_config.name)
-        tuner_ = tuner_(**self.tuner_config.options)
-        tuner_.run(self._get, self._test)
+        self.tuner.run(self._get, self._test)
 
     def _get(self, count):
         support.log(self, 'Generate: {} cases', count)
         return [self.sampler.get() for _ in range(count)]
 
     def _test(self, resource, cases):
-        support.log(self, 'Evaluate: {} cases', len(cases))
+        iteration_count = int(self.resource_scale * resource)
+        support.log(self, 'Evaluate: {} cases, up to {} iterations',
+                    len(cases), iteration_count)
         agents = []
         for case in cases:
             key = _tokenize_case(case)
             agent = self.agents.get(key)
             if agent is None:
-                config = self.learner_config.copy()
+                config = self.config.copy()
                 config.output.path = os.path.join(config.output.path, key)
                 del config.manager['show_address']
                 agent = Agent(self.semaphore, config)
                 self.agents[key] = agent
-            agent.submit(resource)
+            agent.submit(iteration_count)
             agents.append(agent)
-        return [agent.collect(resource) for agent in agents]
+        return [agent.collect(iteration_count) for agent in agents]
 
 
 class Agent:
     def __init__(self, semaphore, config):
         self.semaphore = semaphore
         self.learner = Learner(config)
-        self.results = Agent._load(config.output.path)
+        self.scores = Agent._load(config.output.path)
         self.output_path = config.output.path
         self.lock = threading.Lock()
         self.done = threading.Lock()
 
-    def collect(self, resource):
-        key = _tokenize_resource(resource)
+    def collect(self, iteration_count):
         with self.done:
-            return self.results[key]
+            return self.scores[iteration_count]
 
-    def submit(self, resource):
-        key = _tokenize_resource(resource)
+    def submit(self, iteration_count):
         with self.lock:
-            if key in self.results:
+            if iteration_count in self.scores:
                 return
-            self.results[key] = None
+            self.scores[iteration_count] = None
         self.done.acquire()
-        worker = threading.Thread(target=self._run, args=(resource,),
+        worker = threading.Thread(target=self._run, args=(iteration_count,),
                                   daemon=True)
         worker.start()
 
     def _load(path):
-        results = {}
-        for path in glob.glob(os.path.join(path, 'result-*.txt')):
-            key = re.search('.*result-(.*).txt', path).group(1)
-            results[key] = float(open(path).read())
-            support.log(Agent, 'Result: {}', path)
-        return results
+        scores = {}
+        for path in glob.glob(os.path.join(path, 'score-*.txt')):
+            iteration_count = re.search('.*score-(.*).txt', path).group(1)
+            iteration_count = int(iteration_count)
+            scores[iteration_count] = float(open(path).read())
+            support.log(Agent, 'Score: {}', path)
+        return scores
 
-    def _save(path, key, result):
-        path = os.path.join(path, 'result-{}.txt'.format(key))
+    def _save(path, iteration_count, score):
+        path = os.path.join(path, 'score-{}.txt'.format(iteration_count))
         with open(path, 'w') as file:
-            file.write('{:.15e}'.format(result))
+            file.write('{:.15e}'.format(score))
 
-    def _run(self, resource):
+    def _run(self, iteration_count):
         with self.semaphore:
-            key = _tokenize_resource(resource)
-            result = 0
-            Agent._save(self.output_path, key, result)
             with self.lock:
-                self.results[key] = result
+                last_iteration_count = max(self.scores.keys())
+            assert(last_iteration_count < iteration_count)
+            support.log(
+                self, 'Learn: start at iteration {}, stop at iteration {}',
+                last_iteration_count, iteration_count)
+            for _ in range(last_iteration_count, iteration_count):
+                self.learner.run()
+            loss = self.learner.run_test()
+            decay = np.reshape(np.exp(-np.arange(len(loss))), loss.shape)
+            score = np.mean((loss * decay)**2)
+            support.log(
+                self, 'Learn: stop at iteration {}, score {}',
+                iteration_count, score)
+            Agent._save(self.output_path, iteration_count, score)
+            with self.lock:
+                self.scores[iteration_count] = score
             self.done.release()
 
 
@@ -109,6 +122,3 @@ def _tokenize_case(case):
         value = str(case[name])
         chunks.append('{}={}'.format(alias, value))
     return ','.join(chunks)
-
-def _tokenize_resource(resource):
-    return str(int(resource))
