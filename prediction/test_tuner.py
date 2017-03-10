c9bd68b6ac219468a064f9c6c1736fc53db685fd
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-10 05:50:24 +0100

    Make a cosmetic adjustment

--- prediction/test_tuner.py
+++ prediction/test_tuner.py
@@ -4,21 +4,21 @@ import unittest
 
 class HyperbandTestCase(unittest.TestCase):
     def test_run(self):
-        observed_ns = []
-        observed_rs = []
-        observed_cs = []
+        observed_n = []
+        observed_r = []
+        observed_c = []
         def _get(n):
-            observed_ns.append(n)
+            observed_n.append(n)
             return list(range(n))
         def _test(r, c):
-            observed_rs.append(int(r))
-            observed_cs.append(len(c))
+            observed_r.append(int(r))
+            observed_c.append(len(c))
             return [r * c for c in c]
         tuner = Hyperband()
         tuner.run(_get, _test)
-        expected_ns = [81, 34, 15, 8, 5]
-        expected_rs = [1, 3, 9, 27, 81, 3, 9, 27, 81, 9, 27, 81, 27, 81, 81]
-        expected_cs = [81, 27, 9, 3, 1, 27, 9, 3, 1, 9, 3, 1, 6, 2, 5]
-        self.assertEqual(expected_ns, observed_ns)
-        self.assertEqual(expected_rs, observed_rs)
-        self.assertEqual(expected_cs, observed_cs)
+        expected_n = [81, 34, 15, 8, 5]
+        expected_r = [1, 3, 9, 27, 81, 3, 9, 27, 81, 9, 27, 81, 27, 81, 81]
+        expected_c = [81, 27, 9, 3, 1, 27, 9, 3, 1, 9, 3, 1, 6, 2, 5]
+        self.assertEqual(expected_n, observed_n)
+        self.assertEqual(expected_r, observed_r)
+        self.assertEqual(expected_c, observed_c)
