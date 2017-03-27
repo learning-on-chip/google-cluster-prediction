6fe0950c35c44eb79d34515556bf2035360042ec
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-27 10:40:45 +0200

    Show all available checkpoints

--- prediction/learner.py
+++ prediction/learner.py
@@ -118,13 +118,29 @@ class Checkpoint:
         paths = Checkpoint._load(self.path)
         if len(paths) == 0:
             return
-        path = paths[np.max(list(paths.keys()))]
-        should = self.auto
-        if should is None:
-            answer = input('Restore "{}"? '.format(path))
-            should = not answer.lower().startswith('n')
-        if not should:
+        step_counts = sorted(list(paths.keys()))
+        if self.auto is None:
+            print('Choose one of the following options:')
+            print('    0. Start anew')
+            for i, step_count in enumerate(step_counts):
+                print('    {}. Load {}'.format(i + 1, paths[step_count]))
+            while True:
+                try:
+                    i = int(input('Your choice: '))
+                except ValueError:
+                    continue
+                if i < 0 or i > len(step_counts):
+                    continue
+                elif i == 0:
+                    return
+                else:
+                    i -= 1
+                    break
+        elif self.auto is True:
+            i = -1
+        else:
             return
+        path = paths[step_counts[i]]
         self.saver.restore(session, path)
         support.log(self, 'Restore: {}', path)
 
