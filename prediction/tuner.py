8d92ba889d42337a6219ceafb40363e5a6e79668
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-28 11:11:37 +0200

    Keep track of the best in Hyperband

--- prediction/tuner.py
+++ prediction/tuner.py
@@ -10,6 +10,7 @@ class Hyperband:
         self.count = int(np.log(resource) / np.log(eta)) + 1
 
     def run(self, generate, assess):
+        best = None
         for i in reversed(range(self.count)):
             r = self.resource * self.eta**(-i)
             n = int(np.ceil(self.count // (i + 1) * self.eta**i))
@@ -17,5 +18,9 @@ class Hyperband:
             for j in range(i + 1):
                 r_j = r * self.eta**j
                 n_j = n * self.eta**(-j)
-                ranking = np.argsort(assess(r_j, c), kind='mergesort')
-                c = [c[k] for k in ranking[0:int(n_j / self.eta)]]
+                scores = assess(r_j, c)
+                ranking = np.argsort(scores, kind='mergesort')
+                if best is None or scores[ranking[0]] < best[-1]:
+                    best = (c[ranking[0]], r_j, scores[ranking[0]])
+                c = [c[k] for k in ranking[:int(n_j / self.eta)]]
+        return best
