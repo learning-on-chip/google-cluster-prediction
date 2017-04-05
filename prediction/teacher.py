4127971cc9757328ff05dfb2d67828878b68bbaf
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-04-05 15:05:52 +0200

    Exclude “flat” samples from the assessment

--- prediction/teacher.py
+++ prediction/teacher.py
@@ -1,8 +1,11 @@
+from . import support
 import numpy as np
 import tensorflow as tf
 
 
 class Teacher:
+    _EPSILON = np.finfo(np.float32).eps
+
     def __init__(self, model, config):
         self.future_length = config.future_length
         with tf.variable_scope('training_loss'):
@@ -21,28 +24,33 @@ class Teacher:
     def _assess(input, future_length, predict):
         rmse_sum = np.zeros([future_length])
         nrmse_sum = np.zeros([future_length])
-        nrmse_sample_count = 0
-        squared = np.zeros([future_length])
+        flat_count = 0
+        sum = np.zeros([future_length])
         count = np.zeros([future_length], dtype=np.int)
         for sample in range(input.sample_count):
             sample = input.get(sample)
-            sample_length = sample.shape[0]
+            norm = np.amax(sample) - np.amin(sample)
+            if norm < Teacher._EPSILON:
+                flat_count += 1
+                continue
+            sample_length, dimension_count = sample.shape
             y_hat = predict(sample, future_length)
-            squared.fill(0)
+            sum.fill(0)
             count.fill(0)
             for i in range(sample_length):
                 length = min(sample_length - (i + 1), future_length)
                 deviation = sample[(i + 1):(i + 1 + length), :] - \
                             y_hat[i, :length, :]
-                squared[:length] += np.sum(deviation**2, axis=-1)
-                count[:length] += 1
-            rmse = np.sqrt(squared / count)
+                sum[:length] += np.sum(deviation**2, axis=-1)
+                count[:length] += dimension_count
+            rmse = np.sqrt(sum / count)
             rmse_sum += rmse
-            norm = np.amax(sample) - np.amin(sample)
-            if norm > 0:
-                nrmse_sample_count += 1
-                nrmse_sum += rmse / norm
+            nrmse_sum += rmse / norm
+        if flat_count > 0:
+            support.log(
+                Teacher, 'Flat samples: {}',
+                support.format_percentage(flat_count, input.sample_count))
         return {
-            'MRMSE': rmse_sum / input.sample_count,
-            'MNRMSE': nrmse_sum / nrmse_sample_count,
+            'MRMSE': rmse_sum / (input.sample_count - flat_count),
+            'MNRMSE': nrmse_sum / (input.sample_count - flat_count),
         }
