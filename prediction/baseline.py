24191b7b0c68202be8024772306172c739948b52
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-04-13 10:04:30 +0200

    Refactor Baseline a bit

--- prediction/baseline.py
+++ prediction/baseline.py
@@ -6,12 +6,6 @@ class Baseline(Learner):
     def __init__(self, *arguments):
         super(Baseline, self).__init__(*arguments)
 
-    def run_test(self):
-        return self.teacher.assess(self.input.test, self._assess)
-
-    def run_validation(self):
-        return self.teacher.assess(self.input.validation, self._assess)
-
     def _assess(self, sample, future_length):
         sample_length, dimension_count = sample.shape
         y_hat = np.empty([sample_length, future_length, dimension_count])
@@ -19,3 +13,6 @@ class Baseline(Learner):
             for j in range(future_length):
                 y_hat[i, j, :] = sample[i, :]
         return y_hat
+
+    def _run_assessment(self, target):
+        return self.teacher.assess(getattr(self.input, target), self._assess)
