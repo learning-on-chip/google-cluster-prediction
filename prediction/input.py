148be7e82b76b1fdad9c3d6c66ef2670846352cf
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-04-29 10:08:36 +0200

    Fix a typo

--- prediction/input.py
+++ prediction/input.py
@@ -153,7 +153,7 @@ class Instance:
             else:
                 x = tf.reshape(data, [-1, self.dimension_count])
                 _, outputs = tf.contrib.training.bucket_by_sequence_length(
-                    tf.shape(x)[0], [x], batch_size, config.buckets,
+                    tf.shape(x)[0], [x], self.batch_size, config.buckets,
                     dynamic_pad=True)
                 self.x = outputs[0]
         with tf.variable_scope('y'):
